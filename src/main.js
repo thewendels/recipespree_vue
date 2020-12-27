32d1707c7804e986cfeae0d3927702d629242f85
Wendy <69648973+thewendels@users.noreply.github.com>
2020-12-27 14:26:38 -0600

    adds Recipes Index sort via dropdown

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from "./router";
 import axios from "axios";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Vue2Filters from 'vue2-filters';
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
-
+  
 Vue.config.productionTip = false;
+
+Vue.use(Vue2Filters);
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recipes-index">
     <h1>All Recipes</h1>
-    <div v-for="recipe in recipes">
+    <h4>Sort</h4>
+    <select v-model.lazy="selected">
+      <option disabled value="">Please select one:</option>
+      <option value="alpha">A-Z</option>
+      <option value="revAlpha">Z-A</option>
+      <option value="newest">Newest</option>
+      <option value="oldest">Oldest</option>
+    </select>
+    <div v-for="recipe in orderBy(recipes, sortKey, sortOrder)">
       <h2>Name: {{ recipe.name }}</h2>
       <h3>Source: {{ recipe.source }}</h3>
       <h4 v-if="recipe.tags.length !== 0">Tags</h4>
@@ -24,14 +32,17 @@ img
 
 <script>
 import axios from "axios";
+import Vue2Filters from 'vue2-filters';
 export default {
   data: function() {
     return {
       tag: {},
       recipe: {},
       recipes: [],
+      selected: "",
     };
   },
+  mixins: [Vue2Filters.mixin],
   created: function() {
     axios
       .get("/api/recipes")
@@ -40,6 +51,27 @@ export default {
         this.recipes = response.data;
       });
   },
-  methods: {},
+  computed: {
+    sortKey: function() {
+      if (this.selected === "alpha" || this.selected === "revAlpha") {
+        return "name";
+      } else if (this.selected === "newest" || this.selected === "oldest") {
+        return "id";
+      } else {
+        return "";
+      }   
+    },
+    sortOrder: function() {
+      if (this.selected === "alpha" || this.selected === "oldest") {
+        return 1;
+      } else if (this.selected === "revAlpha" || this.selected === "newest") {
+        return -1;
+      } else {
+        return "";
+      }    
+    },
+  },
+  methods: {
+  },
 };
 </script>
