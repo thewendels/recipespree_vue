7e40e345f2c8a10e31ad274c8e22decab85a4eaf
Wendy <69648973+thewendels@users.noreply.github.com>
2021-01-07 13:04:58 -0600

    adds handling for no search results on Recipes Index

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -25,33 +25,38 @@
         <span class="sr-only">Loading...</span>
       </div>
     </div>
-    
-    <!-- Cards (if user has recipes) -->
-    <div class="container" v-if="isLoaded && recipes.length > 0">
+
+    <!-- Cards (search results and cards) -->
+    <div class="container">
       <!-- Search Results -->
-      <p v-if="this.$route.query.search">Search results for: "{{ this.$route.query.search }}"</p>
+      <div class="m-3">
+        <p v-if="this.$route.query.search && recipes.length == 0">There are no search results for: "{{ this.$route.query.search }}"</p>
+        <p v-else-if="this.$route.query.search">Search results for: "{{ this.$route.query.search }}"</p>
+      </div>
       <!-- Cards -->
-      <div class="row">
-        <div class="col-12 col-lg-4 mb-4" v-for="recipe in orderBy(recipes, sortKey, sortOrder)">
-          <router-link v-bind:to="`/recipes/${recipe.id}`">
-            <div class="card b-0 shadow-primary-xs shadow-primary-md-hover transition-all-ease-250 transition-hover-top h3-100 rounded overflow-hidden h-100">
-              <div class="clearfix">
-                <img v-if="recipe.image_url" class="card-img" :src="`${recipe.image_url}`" v-bind:alt="recipe.name">
-                <img v-else class="card-img" src="/assets/images/default-recipe-photo.png" v-bind:alt="recipe.name">
+      <div v-if="isLoaded && recipes.length > 0">
+        <div class="row">
+          <div class="col-12 col-lg-4 mb-4" v-for="recipe in orderBy(recipes, sortKey, sortOrder)">
+            <router-link v-bind:to="`/recipes/${recipe.id}`">
+              <div class="card b-0 shadow-primary-xs shadow-primary-md-hover transition-all-ease-250 transition-hover-top h3-100 rounded overflow-hidden h-100">
+                <div class="clearfix">
+                  <img v-if="recipe.image_url" class="card-img" :src="`${recipe.image_url}`" v-bind:alt="recipe.name">
+                  <img v-else class="card-img" src="/assets/images/default-recipe-photo.png" v-bind:alt="recipe.name">
+                </div>
+                <div class="card-body font-weight-light">
+                  <h5 style="color: #212529">{{ recipe.name }}</h5>
+                  <p>{{ recipe.source }}</p>
+                  <router-link v-if="recipe.tags.length !== 0" v-for="tag in recipe.tags" class="btn btn-sm btn-outline-pink btn-pill mb-1 mr-1 ml-0" style="font-size:0.75rem" v-bind:to="`/tags/${tag.id}`" :key="tag.id">{{ tag.name }}</router-link>
+                </div>
               </div>
-              <div class="card-body font-weight-light">
-                <h5 style="color: #212529">{{ recipe.name }}</h5>
-                <p>{{ recipe.source }}</p>
-                <router-link v-if="recipe.tags.length !== 0" v-for="tag in recipe.tags" class="btn btn-sm btn-outline-pink btn-pill mb-1 mr-1 ml-0" style="font-size:0.75rem" v-bind:to="`/tags/${tag.id}`" :key="tag.id">{{ tag.name }}</router-link>
-              </div>
-            </div>
-          </router-link>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
 
     <!-- Message (if user has no recipes) -->
-    <div v-if="isLoaded && recipes.length == 0" class="my-5 mx-3 d-flex align-items-center justify-content-center">
+    <div v-if="isLoaded && this.$route.query.search == '' && recipes.length == 0" class="my-5 mx-3 d-flex align-items-center justify-content-center">
       <h5 class="mx-3 align-items-start" >Welcome to RecipeSpree!</h5>
       <div>
         <p>To get started, click on <strong>Add a Recipe</strong> above. You can fill in your recipe's details one field at a time, or import the recipe by entering its URL.</p>
