ec5b70146bcbf754608027049fb1d79154a0e400
Wendy <69648973+thewendels@users.noreply.github.com>
2021-01-31 13:17:52 -0600

    updates main.js to set the API base URL for production

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import Vue2Filters from 'vue2-filters';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://aqueous-shore-85763.herokuapp.com/";
   
 Vue.config.productionTip = false;
 
